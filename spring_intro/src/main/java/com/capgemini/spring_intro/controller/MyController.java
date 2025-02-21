da8d7f5eb2a7d10d8a3b28fb6bb7e51d3a88b234
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-22 02:54:08 +0530

    [Himanshu Ranjan] Add. hello using request parameter

--- spring_intro/src/main/java/com/capgemini/spring_intro/controller/MyController.java
+++ spring_intro/src/main/java/com/capgemini/spring_intro/controller/MyController.java
@@ -3,6 +3,7 @@ package com.capgemini.spring_intro.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class MyController {
     @GetMapping("/hello")
     public ResponseEntity<String> getHello(){
         return new ResponseEntity<>("Hello from bridgelabz", HttpStatus.OK);
+    }
+
+    @GetMapping("/hello/query")
+    public ResponseEntity<String> getHello(@RequestParam String name){
+        return new ResponseEntity<>("Hello " + name + " from bridgelabz", HttpStatus.OK);
     }
 }
